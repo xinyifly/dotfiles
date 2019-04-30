05f2e672635556bd956bf5aa6ad57f8179314147
Zeyu Chen <xinyifly@live.cn>
2019-04-30 15:19:16 +0800

    feat(emacs): add melpa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,6 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+(require 'init-prelude)
 (require 'init-misc)
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
