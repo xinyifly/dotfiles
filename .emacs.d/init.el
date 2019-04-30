529370071fbde2643257241ed457bff137407931
Zeyu Chen <xinyifly@live.cn>
2019-04-30 15:02:44 +0800

    feat(emacs): add misc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,7 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+(require 'init-misc)
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
---
+++ .emacs.d/lisp/init-misc.el
@@ -0,0 +1,10 @@
+(setq make-backup-files nil)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(set-face-font 'default "WenQuanYi Zen Hei Mono-12")
+(setq split-width-threshold 120)
+
+(provide 'init-misc)
