469591381e91017ad4a55527fb337f933766209f
Zeyu Chen <xinyifly@live.cn>
2019-04-30 14:40:22 +0800

    feat: add emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
