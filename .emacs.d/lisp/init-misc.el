37381fdabc287c1d60eb38d498cdab46c650a3be
Zeyu Chen <xinyifly@live.cn>
2019-06-27 17:12:57 +0800

    feat(emacs/misc): add packages
    projectile-rails, flycheck, password-store, restclient

--- .emacs.d/lisp/init-misc.el
+++ .emacs.d/lisp/init-misc.el
@@ -15,11 +15,20 @@
 (projectile-mode 1)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
+(package-install 'projectile-rails)
+(projectile-rails-global-mode 1)
+
 (package-install 'magit)
 (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
 (setq magit-diff-refine-hunk t)
 (setq magit-delete-by-moving-to-trash nil)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch)
+
+(package-install 'flycheck)
+(global-flycheck-mode 1)
+
+(package-install 'password-store)
+(package-install 'restclient)
 
 (provide 'init-misc)
